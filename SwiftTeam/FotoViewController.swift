3fda0aca447189d19cc3f11661788c16295ac0ea
Alberto Estrada <a01276671@itesm.mx>
2022-08-30 18:51:53 -0500

    Simis

--- SwiftTeam/FotoViewController.swift
+++ SwiftTeam/FotoViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class FotoViewController: UIViewController {
+    
+    
+    @IBAction func regresar(_ sender: Any) {
 
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
